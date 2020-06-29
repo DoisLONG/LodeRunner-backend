82b92bbf14938a01c736d343f328a016c417b8d6
DoisLONG <15221580643@163.com>
2020-06-29 17:33:08 +0800

    initialization

---
+++ src/main/java/loderunner/ch/ChApplication.java
@@ -0,0 +1,16 @@
+package loderunner.ch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ChApplication.class, args
+		);
+
+		System.out.print("hello");
+	}
+}
